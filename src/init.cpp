5b9b1c12f0bbd2a4806ea9a100a027a53e413a77
lopeed <lopeed@hotmail.com>
2021-01-16 19:39:08 +0100

    update About screen

--- src/init.cpp
+++ src/init.cpp
@@ -573,6 +573,8 @@ std::string LicenseInfo()
            "\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2018-%i The DAPS Project developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2020-%i The PRCY developers"), COPYRIGHT_YEAR)) + "\n" +
+           "\n" +
            FormatParagraph(_("This is experimental software.")) + "\n" +
            "\n" +
            FormatParagraph(_("Distributed under the MIT software license, see the accompanying file COPYING or <http://www.opensource.org/licenses/mit-license.php>.")) + "\n" +
