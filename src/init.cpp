ee9aa3e79f1c2f6ad78d697ab95a469b175be6b2
lopeed_prcy <35898975+lopeed@users.noreply.github.com>
2021-06-04 09:49:43 +0200

    init: pwalletMain access without being guarded by the ENABLE_WALLET config flag

--- src/init.cpp
+++ src/init.cpp
@@ -1721,6 +1721,7 @@ bool AppInit2(bool isDaemon)
     //get the mode of budget voting for this masternode
     strBudgetMode = GetArg("-budgetvotemode", "auto");
 
+#ifdef ENABLE_WALLET
     if (GetBoolArg("-mnconflock", true) && pwalletMain) {
         LOCK(pwalletMain->cs_wallet);
         LogPrintf("Locking Masternodes:\n");
@@ -1732,6 +1733,7 @@ bool AppInit2(bool isDaemon)
             pwalletMain->LockCoin(outpoint);
         }
     }
+#endif
 
     fEnableSwiftTX = GetBoolArg("-enableswifttx", fEnableSwiftTX);
     nSwiftTXDepth = GetArg("-swifttxdepth", nSwiftTXDepth);
